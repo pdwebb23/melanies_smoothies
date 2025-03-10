8eeb5167fcac97e8043a9afa70fd38de50925cb0
pdwebb23 <philip.webb@clarionhg.com>
2025-03-10 11:42:06 +0000

    Update order_form.py

--- order_form.py
+++ order_form.py
@@ -51,5 +51,5 @@ if ingredients_list:
 # New Section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json()), use_container_width=True)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
